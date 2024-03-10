5dc0aaf7134e2df471777af35096971bfcfd517f
KappaPrajd <tomaszmolski22@wp.pl>
2024-03-10 20:08:36 +0100

    refactor: moved GradientBox to components

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,5 @@
+import GradientBox from '../../components/GradientBox';
 import AdvertBar from '../../views/AdvertBar';
-import GradientBox from '../../views/GradientBox';
 import HeroSection from '../../views/HeroSection';
 import Navbar from '../../views/Navbar';
 import s from './Home.module.css';
