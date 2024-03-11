3583e0d73d4c16246e274e2931295b9a23cbf9cf
KappaPrajd <tomaszmolski22@wp.pl>
2024-03-11 16:28:52 +0100

    improvement: styled icon semantic html

--- src/components/StyledIcon/index.tsx
+++ src/components/StyledIcon/index.tsx
@@ -3,7 +3,7 @@ import { PropsWithChildren } from 'react';
 import s from './StyledIcon.module.css';
 
 const StyledIcon = ({ children }: PropsWithChildren) => {
-  return <div className={s.container}>{children}</div>;
+  return <button className={s.container}>{children}</button>;
 };
 
 export default StyledIcon;
